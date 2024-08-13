c90f51801f77061abef84b306f7ec09ff0d4a674
Abigo <matheusgsr8@gmail.com>
2024-08-13 00:40:26 -0300

    Modificações Estéticas

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Ficar Careca
 document.addEventListener("DOMContentLoaded", (event) => {
   const imagem = document.getElementById("imagem");
   const areaClicavel = { x: 0, y: 0, width: 50, height: 12 };
@@ -18,7 +19,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
   });
 
   function trocarImagem() {
-    imagem.src = "caiaocareca.png";
+    imagem.src = "./img/caiaocareca.png";
   }
 });
 var gameData = {
@@ -27,11 +28,13 @@ var gameData = {
   mmrPerClickCost: 10,
 };
 
+// Jogar Campos
 function playCampos() {
   gameData.mmr += gameData.mmrPerClick;
   document.getElementById("mmr").innerHTML = gameData.mmr;
 }
 
+// Aumentar QI
 function studyCampos() {
   if (gameData.mmr >= gameData.mmrPerClickCost) {
     gameData.mmr -= gameData.mmrPerClickCost;
@@ -41,25 +44,41 @@ function studyCampos() {
     document.getElementById("perClickUpgrade").innerHTML =
       "Aumentar QI (Nível " +
       gameData.mmrPerClick +
-      ") Cost: " +
+      ") Custo: " +
       gameData.mmrPerClickCost +
       " MMR";
   }
 }
+// Salvar
 var mainGameLoop = window.setInterval(function () {
   playCampos();
 }, 1000);
 var saveGameLoop = window.setInterval(function () {
   localStorage.setItem("caiaoClickerSave", JSON.stringify(gameData));
-}, 15000);
+}, 5000);
 var savegame = JSON.parse(localStorage.getItem("caiaoClickerSave"));
 if (savegame !== null) {
   gameData = savegame;
   document.getElementById("perClickUpgrade").innerHTML =
-    "Aumentar QI (Currently Level " +
+    "Aumentar QI (Nível " +
     gameData.mmrPerClick +
-    ") Cost: " +
+    ") Custo: " +
     gameData.mmrPerClickCost +
     " MMR";
   document.getElementById("mmr").innerHTML = gameData.mmr;
+}
+// Barra de progresso
+function move() {
+  var elem = document.getElementById("myBar");
+  var width = 20;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++;
+      elem.style.width = width + "%";
+      elem.innerHTML = width * 1 + "%";
+    }
+  }
 }
